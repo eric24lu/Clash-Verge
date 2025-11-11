d489354b715213652cd93c54b8f71f78b03190b0
eric lu <46375416+eric24lu@users.noreply.github.com>
2025-11-11 14:17:59 +0800

    更新 Script.js

--- Script.js
+++ Script.js
@@ -1,19 +1,22 @@
 function main(config) {
-    // 根据名称排除节点 - 排除包含 "Premium" 的节点
-    removeNodeByName(config, /Premium/i);
+    // 根据名称排除节点
+    removeNodeByName(config, /Premium/g);
     return config;
 }
 
+/**
+ * 根据名称排除节点
+ */
 function removeNodeByName(config, regExp) {
-    // 过滤 proxies 数组
-    if (config.proxies) {
+    // 排除 proxies 中匹配的节点
+    if (config.proxies && Array.isArray(config.proxies)) {
         config.proxies = config.proxies.filter(proxy => !proxy.name.match(regExp));
     }
 
-    // 过滤 proxy-groups 中的代理引用
-    if (config['proxy-groups']) {
+    // 从 proxy-groups 中移除对应节点的引用
+    if (config['proxy-groups'] && Array.isArray(config['proxy-groups'])) {
         config['proxy-groups'] = config['proxy-groups'].map(group => {
-            if (group.proxies) {
+            if (group.proxies && Array.isArray(group.proxies)) {
                 group.proxies = group.proxies.filter(name => !name.match(regExp));
             }
             return group;
